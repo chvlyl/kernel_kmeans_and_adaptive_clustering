bcebbbf0fb17132200bb266120f830c6d9671060
Eric Z Chen <chvlyl@gmail.com>
2017-11-27 20:15:13 -0500

    init package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='kernel_kmeans_and_adaptive_clustering',
+      version='0.0.1',
+      description='kernel kmeans and adaptive clustering',
+      author='Eric',
+      url='https://github.com/chvlyl/kernel_kmeans_and_adaptive_clustering',
+      packages=['kmean_clustering'],
+     )
